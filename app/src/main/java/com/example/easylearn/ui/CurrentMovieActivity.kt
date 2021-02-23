d96e6c8fca57044d45f45f8c647bff87018cd187
Vortex Laptop <v12ten@gmx.com>
2021-02-23 19:50:04 +0300

    current and list screens fixed

--- app/src/main/java/com/example/easylearn/ui/CurrentMovieActivity.kt
+++ app/src/main/java/com/example/easylearn/ui/CurrentMovieActivity.kt
@@ -1,15 +1,14 @@
 package com.example.easylearn.ui
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.example.easylearn.Data
 import com.example.easylearn.R
+import com.example.easylearn.databinding.ActivityCurrentMovieBinding
 import com.example.easylearn.entities.OmdbMovieDetails
 import com.example.easylearn.presentation.CurrentMoviePresenter
 import com.example.easylearn.presentation.CurrentMovieView
-import kotlinx.android.synthetic.main.activity_current_movie.*
 import moxy.MvpAppCompatActivity
 import moxy.presenter.InjectPresenter
 
@@ -17,20 +16,22 @@ class CurrentMovieActivity : MvpAppCompatActivity(), CurrentMovieView {
 
     @InjectPresenter
     lateinit var presenter: CurrentMoviePresenter
+    lateinit var binding: ActivityCurrentMovieBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_current_movie)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_current_movie)
         presenter.loadMovieDetails(Data.imdbId)
     }
 
     override fun showMovieData(result: OmdbMovieDetails) {
         Glide.with(this)
             .load(result.poster)
-            .into(current_movie_image_poster)
-        current_movie_text_title.text = result.title
-        current_movie_text_year.text = result.year.toString()
-        current_movie_text_genre.text = result.genre
-        current_movie_text_director.text = result.director
-        current_movie_text_plot.text = result.plot
+            .into(binding.currentMovieImagePoster)
+        binding.currentMovieTextTitle.text = result.title
+        binding.currentMovieTextYear.text = result.year.toString()
+        binding.currentMovieTextGenre.text = result.genre
+        binding.currentMovieTextDirector.text = result.director
+        binding.currentMovieTextPlot.text = result.plot
     }
 }
--- app/src/main/java/com/example/easylearn/ui/MovieListActivity.kt
+++ app/src/main/java/com/example/easylearn/ui/MovieListActivity.kt
@@ -2,16 +2,15 @@ package com.example.easylearn.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.easylearn.Data
 import com.example.easylearn.MovieAdapter
 import com.example.easylearn.R
+import com.example.easylearn.databinding.ActivityMovieListBinding
 import com.example.easylearn.entities.OmdbMovie
 import com.example.easylearn.presentation.MovieListPresenter
 import com.example.easylearn.presentation.MovieListView
-import com.google.gson.Gson
-import kotlinx.android.synthetic.main.activity_movie_list.*
 import moxy.MvpAppCompatActivity
 import moxy.presenter.InjectPresenter
 
@@ -20,19 +19,20 @@ class MovieListActivity : MvpAppCompatActivity(), MovieListView {
     @InjectPresenter
     lateinit var presenter: MovieListPresenter
     lateinit var adapter: MovieAdapter
+    lateinit var binding: ActivityMovieListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_list)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_movie_list)
 
         adapter = MovieAdapter(this, onItemClick = { items: List<OmdbMovie>?, position: Int ->
             presenter.onItemClicked(items, position)
         })
-        movie_list_button_search.setOnClickListener {
-            presenter.onSearchClicked(movie_list_edit_search.text.toString())
+        binding.movieListButtonSearch.setOnClickListener {
+            presenter.onSearchClicked(binding.movieListEditSearch.text.toString())
         }
-        movie_list_recycler_movies.layoutManager = LinearLayoutManager(this)
-        movie_list_recycler_movies.adapter = adapter
+        binding.movieListRecyclerMovies.layoutManager = LinearLayoutManager(this)
+        binding.movieListRecyclerMovies.adapter = adapter
     }
 
     override fun toCurrentMovie(id: String) {
