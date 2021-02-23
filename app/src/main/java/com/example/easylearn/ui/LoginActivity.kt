fad23fcdd21c84ec9c46f0de7dd6d2c15b3a7990
Vortex Laptop <v12ten@gmx.com>
2021-02-23 19:42:26 +0300

    login screen fixed

--- app/src/main/java/com/example/easylearn/ui/LoginActivity.kt
+++ app/src/main/java/com/example/easylearn/ui/LoginActivity.kt
@@ -4,42 +4,44 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.example.easylearn.R
+import com.example.easylearn.databinding.ActivityLoginBinding
 import com.example.easylearn.presentation.LoginPresenter
 import com.example.easylearn.presentation.LoginView
-import kotlinx.android.synthetic.main.activity_login.*
 import moxy.MvpAppCompatActivity
 import moxy.presenter.InjectPresenter
 
 class LoginActivity : MvpAppCompatActivity(), LoginView {
+
     @InjectPresenter
     lateinit var presenter: LoginPresenter
+    lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
-        login.setOnClickListener {
+        binding.login.setOnClickListener {
             presenter.loginClick()
         }
-        sign.setOnClickListener {
-            presenter.signClick(email.text.toString(), password.text.toString())
+        binding.sign.setOnClickListener {
+            presenter.signClick(binding.email.text.toString(), binding.password.text.toString())
         }
     }
-
 
     override fun showMessage() {
         Toast.makeText(this, getString(R.string.error), Toast.LENGTH_SHORT).show()
     }
 
-
     override fun switchViews() {
-        description.visibility = View.GONE
-        free.visibility = View.GONE
-        login.visibility = View.GONE
-        email.visibility = View.VISIBLE
-        password.visibility = View.VISIBLE
-        sign.visibility = View.VISIBLE
-        facebook.visibility = View.VISIBLE
+        binding.description.visibility = View.GONE
+        binding.free.visibility = View.GONE
+        binding.login.visibility = View.GONE
+        binding.email.visibility = View.VISIBLE
+        binding.password.visibility = View.VISIBLE
+        binding.sign.visibility = View.VISIBLE
+        binding.facebook.visibility = View.VISIBLE
     }
 
     override fun toMainScreen() {
